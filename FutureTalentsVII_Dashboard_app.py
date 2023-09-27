3082f8bb754a4d9fdf6092d7542eaa4ae32f6d4e
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-09-26 20:47:36 -0500

    Add files via upload

---
+++ FutureTalentsVII_Dashboard_app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 20:46:32 2023
+
+@author: Freddy J. Orozco R.
+"""
+
+import streamlit as st
+import hydralit_components as hc
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
+import pandas as pd
+import numpy as np
+
+
