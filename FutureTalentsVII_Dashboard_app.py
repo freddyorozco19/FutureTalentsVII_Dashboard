9be2dcb7cc206e936f7848b4a2295809b75bf095
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-10-10 17:42:44 -0500

    Update FutureTalentsVII_Dashboard_app.py

--- FutureTalentsVII_Dashboard_app.py
+++ FutureTalentsVII_Dashboard_app.py
@@ -49,7 +49,7 @@ from streamlit_option_menu import option_menu
 
 # 1. as sidebar menu
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+    selected = option_menu("Main Menu", ["Rankings", 'Player Search'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
 
