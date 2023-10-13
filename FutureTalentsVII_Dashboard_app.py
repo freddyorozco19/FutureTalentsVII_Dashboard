bd3e58ad212440f95b25dd16bd10b3c2a7c14335
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-10-13 16:47:39 -0500

    Update FutureTalentsVII_Dashboard_app.py

--- FutureTalentsVII_Dashboard_app.py
+++ FutureTalentsVII_Dashboard_app.py
@@ -111,7 +111,6 @@ if selected == "Player Search":
     
     #Data
     df = pd.read_excel("MatchesData/matches.xlsx")
-
     st.title("Search Player")
 
     df['FieldXfrom'] = (df['FieldXfrom']*105)/1
@@ -144,30 +143,33 @@ if selected == "Player Search":
     #  PlayerSel = st.selectbox("Choose player:", Player_Lst)
     #  #df = df[df['Players'] == PlayerSel].reset_index(drop=True)
     
-    
-    fig, ax = mplt.subplots(figsize=(8, 8), dpi = 800)
-    ax.axis("off")
-    fig.patch.set_visible(False)
-    pitch = Pitch(pitch_color='None', pitch_type='custom', line_zorder=1, linewidth=0.5, goal_type='box', pitch_length=105, pitch_width=68)
-    pitch.draw(ax=ax)
-    #Adding directon arrow
-    ax29 = fig.add_axes([0.368,0.22,0.3,0.05])
-    ax29.axis("off")
-    ax29.set_xlim(0,10)
-    ax29.set_ylim(0,10)
-    ax29.annotate('', xy=(2, 6), xytext=(8, 6), arrowprops=dict(arrowstyle='<-', ls= '-', lw = 1, color = (1,1,1,0.5)))
-    ##ax29.annotate(s='', xy=(2, 5), xytext=(8, 5), arrowprops=dict(arrowstyle='<-', ls= '-', lw = 1, color = (1,1,1,0.5)))
-    ax29.text(5, 2, 'Dirección campo de juego', fontproperties=prop3, c=(1,1,1,0.5), fontsize=10, ha='center')
-    #Adding winstats logo
-    ax53 = fig.add_axes([0.82, 0.14, 0.05, 0.05])
-    url53 = "https://i.postimg.cc/R0QjGByL/sZggzUM.png"
-    response = requests.get(url53)
-    img = Image.open(BytesIO(response.content))
-    ax53.imshow(img)
-    ax53.axis("off")
-    ax53.set_facecolor("#000")
-    #st.dataframe(dfDOWN)
-    ###df = df[(df['EfectiveMinute'] >= EfectMinSel[0]) & (df['EfectiveMinute'] <= EfectMinSel[1])]
-    dfKK = df
-    ax.scatter(df['FieldXfrom'], df['FieldYfrom'], color = "#FF0046", edgecolors='w', s=30, zorder=2, alpha=0.2)
-    st.pyplot(fig, bbox_inches="tight", pad_inches=0.05, dpi=400, format="png")
+    pltmain01, pltmain02 = st.columns(2)
+    with pltmain01:
+        fig, ax = mplt.subplots(figsize=(8, 8), dpi = 800)
+        ax.axis("off")
+        fig.patch.set_visible(False)
+        pitch = Pitch(pitch_color='None', pitch_type='custom', line_zorder=1, linewidth=0.5, goal_type='box', pitch_length=105, pitch_width=68)
+        pitch.draw(ax=ax)
+        #Adding directon arrow
+        ax29 = fig.add_axes([0.368,0.22,0.3,0.05])
+        ax29.axis("off")
+        ax29.set_xlim(0,10)
+        ax29.set_ylim(0,10)
+        ax29.annotate('', xy=(2, 6), xytext=(8, 6), arrowprops=dict(arrowstyle='<-', ls= '-', lw = 1, color = (1,1,1,0.5)))
+        ##ax29.annotate(s='', xy=(2, 5), xytext=(8, 5), arrowprops=dict(arrowstyle='<-', ls= '-', lw = 1, color = (1,1,1,0.5)))
+        ax29.text(5, 2, 'Dirección campo de juego', fontproperties=prop3, c=(1,1,1,0.5), fontsize=10, ha='center')
+        #Adding winstats logo
+        ax53 = fig.add_axes([0.82, 0.14, 0.05, 0.05])
+        url53 = "https://i.postimg.cc/R0QjGByL/sZggzUM.png"
+        response = requests.get(url53)
+        img = Image.open(BytesIO(response.content))
+        ax53.imshow(img)
+        ax53.axis("off")
+        ax53.set_facecolor("#000")
+        #st.dataframe(dfDOWN)
+        ###df = df[(df['EfectiveMinute'] >= EfectMinSel[0]) & (df['EfectiveMinute'] <= EfectMinSel[1])]
+        dfKK = df
+        ax.scatter(df['FieldXfrom'], df['FieldYfrom'], color = "#FF0046", edgecolors='w', s=30, zorder=2, alpha=0.2)
+        st.pyplot(fig, bbox_inches="tight", pad_inches=0.05, dpi=400, format="png")
+    with pltmain02:
+        st.dataframe(df)
